838abb3cae660ce4c0867c7f6770b15b17cd472b
agusapri <agusaprianto27400@gmail.com>
2022-10-06 01:29:23 +0700

    mengerjakan contoh template sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Agus</li>
+ <li>Nama Belakang : Apri</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan kelapa gading</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Cirebon</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>ngelamun depan teras</li>
+ <li>maen burung</li>
+ </ul>
+</section>
